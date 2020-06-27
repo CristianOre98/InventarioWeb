319608207f36ebac62a8db78a3a1c55b68759527
BrayanNolasco <brayannolasco011@gmail.com>
2020-06-27 15:37:42 -0600

    he corregido errores del controlador producto

--- InventarioWeb/src/java/Controller/Productos.java
+++ InventarioWeb/src/java/Controller/Productos.java
@@ -7,7 +7,9 @@ package Controller;
 
 import DAO.ProductoDAO;
 import DAO.ProductoDAOImplementar;
+import Model.Categoria;
 import Model.Producto;
+import com.sun.org.apache.xalan.internal.xsltc.compiler.util.Type;
 import java.io.IOException;
 import java.io.PrintWriter;
 import javax.servlet.RequestDispatcher;
@@ -34,18 +36,16 @@ public class Productos extends HttpServlet {
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        
+        response.setContentType("text/html;charset=UTF-8");
         
     }
-
-   
-   //Agregar metodo listaCategorias
+    //Agregar metodo listaCategorias
     protected void listaProducto(HttpServletRequest request, HttpServletResponse response)
         throws ServletException, IOException{
         response.setContentType("text/html;charset=UTF-8");
         
         //Instancia a categoria DAO
-        ProductoDAO categoria = new ProductoDAOImplementar();
+        ProductoDAO producto = new ProductoDAOImplementar();
         //crear instacia de session; true para iniciar session
         HttpSession sesion = request.getSession(true);
         sesion.setAttribute("lista", producto.listar());
@@ -53,10 +53,20 @@ public class Productos extends HttpServlet {
         dispatcher.forward(request, response);
         
     }
-    
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        processRequest(request, response);
         String parametro = request.getParameter("opcion");
         if(parametro.equals("crear")){
             String pagina = "/Vistas-Producto/crearProducto.jsp";
@@ -65,62 +75,91 @@ public class Productos extends HttpServlet {
 
         }else if(parametro.equals("listar")){
             this.listaProducto(request, response);
-        }else if(parametro.equals("modificar")){
+        }else if(parametro.equals("editar")){
             //Se efectua el casting o conversión de datos porque lo ingresado en el formulario es texto.
-            int id_producto = Integer.parseInt(request.getParameter("id_pro"));
-            String nom_producto = request.getParameter("nombre_pro");
-            int estado_producto = Integer.parseInt(request.getParameter("estado_pro"));
             
-            String pagina = "/Vistas-Producto/crearProducto.jsp?id_pro="+id_producto+"&&nombre_pro="+nom_producto+"&&estado_pro="+estado_producto+"&&senal=1";
+            int id_producto = Integer.parseInt(request.getParameter("id_pro").replaceAll("\\s*$",""));
+            
+            String nom_producto = request.getParameter("nom_pro");
+            String des_producto = request.getParameter("des_pro");
+            int estado_producto = Integer.parseInt(request.getParameter("estado_pro").replaceAll("\\s*$",""));
+            
+            float stock = Float.parseFloat(request.getParameter("stock_pro").replaceAll("\\s*$",""));
+            float precio = Float.parseFloat(request.getParameter("precio_pro").replaceAll("\\s*$",""));
+            
+            String unidad_de_medida = request.getParameter("unidad_pro");
+            int categoria_id = Integer.parseInt(request.getParameter("categoria_pro").replaceAll("\\s*$",""));
+     
+         String fecha_entrada = request.getParameter("fecha_pro");
+	
+    	
+        	
+                String pagina = "/Vistas-Producto/crearProducto.jsp?id_p="+id_producto+"&&nom_p="
+                    +nom_producto+"&&estado_p="+estado_producto+"&&des_p="+des_producto+"&&stock_p="+stock+
+                    "&&precio_p="+precio+"&&unidad_p="+unidad_de_medida+"&&categoria_p="+categoria_id+
+                    "&&fecha_p="+fecha_entrada+"&&senal=1";
             RequestDispatcher dispatcher = getServletContext().getRequestDispatcher(pagina);
             dispatcher.forward(request, response);
+    	
+            
+           
+           
+            
             
         }else if(parametro.equals("eliminar")){
-            int del_id = Integer.parseInt(request.getParameter("id"));
+            int Producto_id = Integer.parseInt(request.getParameter("id"));
             ProductoDAO producto = new ProductoDAOImplementar();
-            producto.borrarCat(del_id);    
+            producto.borrarPro(Producto_id);    
             this.listaProducto(request, response);
         }
     }
 
-    
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-       // processRequest(request, response);
+        processRequest(request, response);
+        // processRequest(request, response);
         String parametro = request.getParameter("accion");
             
                 
-             Productos producto = new Productos();
+             Producto producto = new Producto();
+             Categoria categoria = new Categoria();
         //Se efectua el casting o conversión de datos porque lo ingresado en el formulario es texto.
-        int id_producto = Integer.parseInt(request.getParameter("id_producto"));
-        String nom_producto = request.getParameter("txtNomproducto");
-        int estado_producto = Integer.parseInt(request.getParameter("txtEstadoProducto"));
         
-        Productos.setId_producto(id_producto);
-        Productos.setNom_producto(nom_producto);
-        Productos.setEstado_producto(estado_producto);
+         int id_producto = Integer.parseInt(request.getParameter("txtId_producto"));
+            String nom_producto = request.getParameter("txtNombreProducto");
+            String des_producto = request.getParameter("txtDescripcionProducto");
+            float stock = Float.parseFloat(request.getParameter("txtStockProducto"));
+            float precio = Float.parseFloat(request.getParameter("txtPrecioProducto"));
+            String unidad_de_medida = request.getParameter("txtUnidadProducto");
+            int estado_producto = Integer.parseInt(request.getParameter("txtEstadoProducto"));
+            int catego = Integer.parseInt(request.getParameter("txtCategoriaProducto"));
+            String fecha_entrada = request.getParameter("txtFechaProducto");
         
-        ProductoDAO guardarCategoria = new ProductoDAOImplementar();
-        guardarCategoria.guardarCat(producto);
+        producto.setId_producto(id_producto);
+        producto.setNom_producto(nom_producto);
+        producto.setDes_producto(des_producto);
+        producto.setPrecio(precio);
+        producto.setStock(stock);
+        producto.setUnidadMedida(unidad_de_medida);
+        producto.setEstado(estado_producto);
+        producto.setFecha_entrada(fecha_entrada);
+        producto.setCategoria_id(catego);
+        
+        ProductoDAO guardarProducto = new ProductoDAOImplementar();
+        guardarProducto.guardarPro(producto);
         
         this.listaProducto(request, response);
-        //se efectua el casting o conversion de datos porque lo ingresado en el formulario es texto
-       
-         
-             
-        
-        
-        
-        
-        
-      
-       
-        
-        
-        
-        
     }
+
     /**
      * Returns a short description of the servlet.
      *
@@ -130,7 +169,5 @@ public class Productos extends HttpServlet {
     public String getServletInfo() {
         return "Short description";
     }// </editor-fold>
-
-    
 
 }
